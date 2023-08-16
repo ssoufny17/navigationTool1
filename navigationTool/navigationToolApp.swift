a737bb010c180fe34bdd3fa22071d4002c7eef58
Sarah Soufny <ysoufny1@gmail.com>
2023-08-16 19:09:49 -0400

    Initial Commit

---
+++ navigationTool/navigationToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationToolApp.swift
+//  navigationTool
+//
+//  Created by Sarah Soufny on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct navigationToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
